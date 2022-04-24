21a10e3145a8b7a3c48e6a6ea06f906ae95f5e55
steve jones <sjones@sqlservercentral.com>
2022-04-24 17:38:56 -0600

    Changes requested by Jira #323

--- GlennBerry_Query 8_ServiceAccount.sql
+++ GlennBerry_Query 8_ServiceAccount.sql
@@ -1,3 +1,4 @@
+select 'Diangnostic query 8'
 -- Tells you the account being used for the SQL Server Service and the SQL Agent Service
 -- Shows the process_id, when they were last started, and their current status
 -- Also shows whether you are running on a failover cluster instance, and what node you are running on
--- StoredProcedures/GetOne.sql
+++ StoredProcedures/GetOne.sql
@@ -1,3 +1,3 @@
 CREATE PROCEDURE GetOne
 as 
-select 1 as One
+select 1 as OneNumber
